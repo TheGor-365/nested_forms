db0c5f29b85da89a1c60092d8ca4755c8ed0e5ae
Gor <7495500@gmail.com>
2023-08-22 17:02:15 +0300

    Add accepts_nested_attributes_for

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,5 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :description
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :books
   has_one  :profile
+
+  accepts_nested_attributes_for :books, reject_if: proc { |attributes| attributes['title'].blank? }
+  accepts_nested_attributes_for :profile, update_only: true, allow_destroy: true
 end
